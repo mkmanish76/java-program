4efe21cd3af81fec18a84a12421c6203adc57287
Manish Kumar <manish98uuu@gmail.com>
2025-05-12 00:42:48 +0530

    Add Two Integers.

---
+++ prog01.java
@@ -0,0 +1,10 @@
+class prog01{
+
+  public static void main(String[] args) {
+    
+    int a = 10;
+    int b = 20;
+    int sum = a+b;
+    System.out.println(a+ " + " + b + " = "  + sum);
+  }
+}
