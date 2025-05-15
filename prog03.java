e6e1c07aeedee1488e0b73d3ae24f0d2bfaa845d
Manish Kumar <manish98uuu@gmail.com>
2025-05-16 00:10:39 +0530

    Subtract Two Integers.

---
+++ prog03.java
@@ -0,0 +1,9 @@
+public class prog03 {
+      public static void main(String[] args) {
+    
+    int a = 10;
+    int b = 05;
+    int sum = a-b;
+    System.out.println(a+ " - " + b + " = "  + sum);
+  }
+}
