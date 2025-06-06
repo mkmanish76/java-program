fcbc55c5e1b6200746e795a929ba561a1f8fd0c0
Manish Kumar <manish98uuu@gmail.com>
2025-06-06 22:47:46 +0530

    Multiplication of two no.

---
+++ prog10.java
@@ -0,0 +1,9 @@
+public class prog10 {
+    public static void main(String[] args) {
+    
+    int a = 25;
+    int b = 4;
+    int sum = a*b;
+    System.out.println(a+ " + " + b + " = "  + sum);
+  }
+}
